40be1df8361f470416b1217424226490da668f80
Lourdes Veros <lourdes.veros.alvarez@gmail.com>
2026-08-22 17:17:48 +0200

    feat: create simple initial measurement consumer

---
+++ aws-consumer/MeasurementCreated.cs
@@ -0,0 +1,14 @@
+﻿namespace aws_publisher;
+
+public class MeasurementCreated
+{
+    public required Guid Id { get; init; }
+
+    public required double Value { get; init; }
+
+    public required string Unit { get; init; }
+
+    public required string Type { get; init; }
+
+    public required DateTime DateOf { get; init; }
+}
---
+++ aws-consumer/Program.cs
@@ -0,0 +1,32 @@
+﻿using Amazon.SQS;
+using Amazon.SQS.Model;
+
+var cts = new CancellationTokenSource();
+var sqsClient = new AmazonSQSClient();
+
+var queueUrlResponse = await sqsClient.GetQueueUrlAsync("measurements");
+
+var receiveMessageRequest = new ReceiveMessageRequest
+{
+    QueueUrl = queueUrlResponse.QueueUrl
+};
+
+while (!cts.IsCancellationRequested)
+{
+    var response = await sqsClient.ReceiveMessageAsync(receiveMessageRequest, cts.Token);
+
+    if (response.Messages is null)
+        continue;
+
+    foreach (var message in response.Messages)
+    {
+        Console.WriteLine($"Message Id: {message.MessageId}");
+        Console.WriteLine($"Message Id: {message.Body}");
+
+        await sqsClient.DeleteMessageAsync(queueUrlResponse.QueueUrl, message.ReceiptHandle, cts.Token);
+    }
+
+    await Task.Delay(3000);
+}
+
+Console.WriteLine();
