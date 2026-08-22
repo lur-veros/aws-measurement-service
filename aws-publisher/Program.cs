18245eae5de8a055c3f43f7fa3053437b639f027
Lourdes Veros <lourdes.veros.alvarez@gmail.com>
2026-08-22 16:58:12 +0200

    feat: create simple initial measurement publisher

---
+++ aws-publisher/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Text.Json;
+using Amazon.SQS;
+using Amazon.SQS.Model;
+using aws_publisher;
+
+var sqsClient = new AmazonSQSClient();
+
+var measurement = new MeasurementCreated
+{
+    Id = Guid.NewGuid(),
+    Value = 23.5,
+    Unit = "Celsius",
+    Type = "Temperature",
+    DateOf = DateTime.UtcNow
+};
+
+
+var queueUrlResponse = await sqsClient.GetQueueUrlAsync("measurements");
+
+var sendMessageRequest = new SendMessageRequest
+{
+    QueueUrl = queueUrlResponse.QueueUrl,
+    MessageBody = JsonSerializer.Serialize(measurement)
+};
+
+var response = await sqsClient.SendMessageAsync(sendMessageRequest);
+
+Console.WriteLine();
