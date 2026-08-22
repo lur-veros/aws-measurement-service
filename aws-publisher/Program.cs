a91c1f753e0d0e3b9f5acc097bf611ba8a1603d0
Lourdes Veros <lourdes.veros.alvarez@gmail.com>
2026-08-22 17:02:13 +0200

    feat: add attritubes to the message

--- aws-publisher/Program.cs
+++ aws-publisher/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System.Text.Json;
+using Amazon.Runtime.Internal.Transform;
 using Amazon.SQS;
 using Amazon.SQS.Model;
 using aws_publisher;
@@ -20,7 +21,17 @@ var queueUrlResponse = await sqsClient.GetQueueUrlAsync("measurements");
 var sendMessageRequest = new SendMessageRequest
 {
     QueueUrl = queueUrlResponse.QueueUrl,
-    MessageBody = JsonSerializer.Serialize(measurement)
+    MessageBody = JsonSerializer.Serialize(measurement),
+    MessageAttributes = new Dictionary<string, MessageAttributeValue>
+    {
+        {
+            "MessageType", new MessageAttributeValue
+            {
+                DataType = "String",
+                StringValue = nameof(MeasurementCreated)
+            } 
+        }
+    }
 };
 
 var response = await sqsClient.SendMessageAsync(sendMessageRequest);
